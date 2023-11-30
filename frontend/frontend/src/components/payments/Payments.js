b38074ce1871ae197d3d28274ae62f98a37ea416
Emmanuel Alafonye <ke.alafonye@yahoo.com>
2023-11-30 16:00:10 -0700

    updates on payments

--- frontend/frontend/src/components/payments/Payments.js
+++ frontend/frontend/src/components/payments/Payments.js
@@ -38,13 +38,13 @@ function Payments() {
                 </div>
                 <div class="p-2 w-1/2">
                   <div class="relative">
-                    <label for="email" class="leading-7 text-sm text-gray-600">
+                    <label for="name" class="leading-7 text-sm text-gray-600">
                       Name On Card
                     </label>
                     <input
-                      type="email"
-                      id="email"
-                      name="email"
+                      type="name"
+                      id="name on Card"
+                      name="name on card"
                       class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     />
                   </div>
@@ -60,7 +60,7 @@ function Payments() {
                     </label>
                     <input
                       type="text"
-                      id="name"
+                      id="Last name"
                       name="name"
                       class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     />
@@ -68,13 +68,13 @@ function Payments() {
                 </div>
                 <div class="p-2 w-1/2">
                   <div class="relative">
-                    <label for="email" class="leading-7 text-sm text-gray-600">
+                    <label for="number" class="leading-7 text-sm text-gray-600">
                       Credit Card
                     </label>
                     <input
-                      type="email"
-                      id="email"
-                      name="email"
+                      type="number"
+                      id="number"
+                      name="number"
                       class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     />
                   </div>
