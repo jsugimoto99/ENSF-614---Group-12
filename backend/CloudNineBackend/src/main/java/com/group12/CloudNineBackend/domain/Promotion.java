c79657f434ff12d8d36f849ab3aa91679cce7ee0
awong12345 <awong12345@hotmail.com>
2023-12-01 18:43:04 -0700

    Update promotion.java

--- backend/CloudNineBackend/src/main/java/com/group12/CloudNineBackend/domain/Promotion.java
+++ backend/CloudNineBackend/src/main/java/com/group12/CloudNineBackend/domain/Promotion.java
@@ -1,14 +1,21 @@
 package com.group12.CloudNineBackend.domain;
 
+import java.util.Random;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 
 @Entity
 public class Promotion {
     
-    @Id
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int promoId;
     private String description;
+    private String code; // 5-digit code
     
     // Standard constructors, getters, and setters
     public int getPromoId() {
@@ -19,11 +26,22 @@ public class Promotion {
     	return description;
     }
     
-    public void setPromoId(int promoId) {
-    	this.promoId = promoId;
-    }
-    
     public void setDescription(String description) {
     	this.description = description;
+    }
+    
+    public String getCode() {
+    	return code;
+    }
+    
+    @PrePersist
+    public void generateCode() {
+    	this.code = generateRandomCode();
+    }
+    
+    private String generateRandomCode() {
+        Random random = new Random();
+        int num = 10000 + random.nextInt(90000); // Generates a number between 10000 and 99999
+        return String.valueOf(num);
     }
 }
